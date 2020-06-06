4f7f47f2c727b18facd4f3c57981d7d4b2a9bab9
Apurva <bagaria.apurva@gmail.com>
2020-06-06 14:32:24 +0530

    LeetCode Initialized

---
+++ LeetCode/Easy/1431.cpp
@@ -0,0 +1,13 @@
+vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    int maxElem = *max_element(candies.begin(), candies.end());
+    vector<bool> result;
+    for(int t : candies){
+        if(t>=maxElem || t+extraCandies>=maxElem){
+            result.push_back(true);
+        }
+        else{
+            result.push_back(false);
+        }
+    }
+    return result;
+}
